4f1e2696e56773490b18a8e2dc775295955b2db4
priyankatotala <39249471+priyankatotala@users.noreply.github.com>
2019-09-30 10:29:01 +1000

    Update install.php

--- install.php
+++ install.php
@@ -1,11 +1,11 @@
 <?php
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "1ad16161f1d79d041faa271898ce163f";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "http://localhost/main_controller.php";
 // Build install/approval URL to redirect to
-$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+$install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 // Redirect
 header("Location: " . $install_url);
 die();
