b609e1ec58fe0d07986e6c80bb255d6a7cf6f196
priyankatotala <39249471+priyankatotala@users.noreply.github.com>
2019-10-03 16:17:02 +1000

    Update install.php

--- install.php
+++ install.php
@@ -1,11 +1,10 @@
-<?php
-// Set variables for our request
-$shop = $_GET['shop'];
-$api_key = "1ad16161f1d79d041faa271898ce163f";
-$scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/main_controller.php";
-// Build install/approval URL to redirect to
-$install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
-// Redirect
-header("Location: " . $install_url);
-die();
+
+	
+<form  action="install_app.php" method="post">
+  Shop or Store name: <input name="shop" type="text" " />
+  
+  <input type="submit" name="submit" />
+</form>
+
+
+
